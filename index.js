08ba35010f6dd96e1c1e6fa95442418019d34f55
Tero Keski-Valkama <tero.keski-valkama@cybercom.com>
2016-02-08 19:17:37 +0200

    A typo fixed and serving the svm lib for the frontend as well.

--- index.js
+++ index.js
@@ -4,11 +4,14 @@ const app = express();
 
 // For static files, we will return them.
 app.use(express.static(path.join(__dirname, 'public')));
+app.get('/lib/svm/svm.js', (req, res) => {
+  res.sendFile(path.join(__dirname, 'node_modules/svm/lib/svm.js'));
+});
 
 // For anything else, we will return index.html as is common with
 // single page apps.
 app.use((req, res) => {
-  res.sendFile(path.join(__dirname + 'public/index.html'));
+  res.sendFile(path.join(__dirname, 'public/index.html'));
 });
 
 app.listen(8080);
