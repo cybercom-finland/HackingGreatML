24479e6c48d6d62bd818899023ba907777214c58
Tero Keski-Valkama <tero.keski-valkama@cybercom.com>
2016-02-08 19:24:18 +0200

    Serving datasets to the client.

--- index.js
+++ index.js
@@ -2,11 +2,19 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
+function serve(where, what) {
+  app.get(where, (req, res) => {
+    res.sendFile(path.join(__dirname, what));
+  });
+};
+
 // For static files, we will return them.
 app.use(express.static(path.join(__dirname, 'public')));
-app.get('/lib/svm/svm.js', (req, res) => {
-  res.sendFile(path.join(__dirname, 'node_modules/svm/lib/svm.js'));
-});
+serve('/lib/svm/svm.js', 'node_modules/svm/lib/svm.js');
+serve('/train/indoor.json', 'datasets/sml2010/internalTrainingSet.json');
+serve('/train/outdoor.json', 'datasets/sml2010/externalTrainingSet.json');
+serve('/test/indoor.json', 'datasets/sml2010/internalTestSet.json');
+serve('/test/outdoor.json', 'datasets/sml2010/externalTestSet.json');
 
 // For anything else, we will return index.html as is common with
 // single page apps.
